d9dd2f8971869f3dbadbaabc19319fc8f83e2a6b
Koji Kuwana <koji.kuwana@gmail.com>
2015-10-19 11:02:50 +0200

    add template for java

---
+++ tpl.java
@@ -0,0 +1,12 @@
+import java.lang.*;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+class %TEMPLATE% {
+        public static void main(String[] args){
+                System.out.println("Hello World");
+        }
+}
